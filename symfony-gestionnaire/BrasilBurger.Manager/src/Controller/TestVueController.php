ba68400cd3acf9fcba1b9cd7fa3ce9470d89f2f7
Salim OUEDRAOGO <salimouedz@example.com>
2025-12-28 17:26:41 +0000

    vue statique page dashboard

--- symfony-gestionnaire/BrasilBurger.Manager/src/Controller/TestVueController.php
+++ symfony-gestionnaire/BrasilBurger.Manager/src/Controller/TestVueController.php
@@ -8,13 +8,40 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class TestVueController extends AbstractController
 {
-    #[Route('/test/vue', name: 'app_login')]
-    public function index(): Response
+    #[Route('/test/login', name: 'app_login')]
+    public function login(): Response
     {
         return $this->render('security/login.html.twig', [
             "error" => false,
             "last_username" => null
         ]);
     }
-    
+
+    #[Route('/test/dashboard', name: 'app_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            "error" => false,
+            "last_username" => null
+        ]);
+    }
+
+    #[Route('/test/orders', name: 'app_orders')]
+    public function commandes(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            "error" => false,
+            "last_username" => null
+        ]);
+    }
+
+    #[Route('/test/deliveries', name: 'app_deliveries')]
+    public function livraisons(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            "error" => false,
+            "last_username" => null
+        ]);
+    }
+
 }
