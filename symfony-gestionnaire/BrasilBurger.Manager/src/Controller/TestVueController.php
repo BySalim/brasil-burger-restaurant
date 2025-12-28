3f0adeed5487ba4fb097daf5314260f784853ded
Salim OUEDRAOGO <salimouedz@example.com>
2025-12-28 19:06:29 +0000

    vue statique page détails commande

--- symfony-gestionnaire/BrasilBurger.Manager/src/Controller/TestVueController.php
+++ symfony-gestionnaire/BrasilBurger.Manager/src/Controller/TestVueController.php
@@ -8,40 +8,43 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class TestVueController extends AbstractController
 {
+    # Route défaut en attente de développement
+    #[Route('/test', name: 'app_default')]
+    public function dafault(): Response
+    {
+        return $this->render('test_vue/index.html.twig', []);
+    }
+
     #[Route('/test/login', name: 'app_login')]
     public function login(): Response
     {
-        return $this->render('security/login.html.twig', [
-            "error" => false,
-            "last_username" => null
-        ]);
+        return $this->render('security/login.html.twig', []);
     }
 
     #[Route('/test/dashboard', name: 'app_dashboard')]
     public function dashboard(): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            "error" => false,
-            "last_username" => null
-        ]);
+        return $this->render('dashboard/index.html.twig', []);
     }
 
     #[Route('/test/orders', name: 'app_orders')]
     public function commandes(): Response
     {
-        return $this->render('orders/index.html.twig', [
-            "error" => false,
-            "last_username" => null
-        ]);
+        return $this->render('orders/index.html.twig', []);
+    }
+
+    #[Route('/test/orders/code', name: 'app_details_order')]
+    public function showCommande(): Response
+    {
+        return $this->render('orders/show.html.twig', []);
     }
 
     #[Route('/test/deliveries', name: 'app_deliveries')]
     public function livraisons(): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            "error" => false,
-            "last_username" => null
-        ]);
+        return $this->render('dashboard/index.html.twig', []);
     }
+
+
 
 }
