fd645bc95aa07086fce8cbd3ade303eb129e2213
Salim OUEDRAOGO <salimouedz@example.com>
2026-01-12 16:03:46 +0000

    livraisons pret à être implémenter

--- symfony-gestionnaire/BrasilBurger.Manager/src/Controller/DeliveryController.php
+++ symfony-gestionnaire/BrasilBurger.Manager/src/Controller/DeliveryController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Factory\DeliveriesViewFactory;
 use App\Factory\OrdersViewFactory;
+use App\Form\OrderFilterType;
 use App\Repository\LivraisonRepository;
 use App\ViewModel\DeliveryInfos;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,6 +24,37 @@ class DeliveryController extends AbstractController
     {
     }
 
+    #[Route('/', name: 'app_deliveries')]
+    public function index(Request $request, PaginatorInterface $paginator): Response
+    {
+
+//        $form = $this->createForm(OrderFilterType::class);
+//        $form->handleRequest($request);
+//
+//        $filters = $form->getData();
+//
+//        $queryBuilder = $this->commandeRepository->findAllWithFiltersQB(
+//            search: $filters['search'] ?? null,
+//            date: $filters['date'] ?? null,
+//            type: $filters['type'] ?? null,
+//            status: $filters['status'] ?? null
+//        );
+//
+//        $per_page = $filters['per_page'] ?? $this->params->get('app.pagination.default_per_page');
+//        $pagination = $paginator->paginate(
+//            $queryBuilder,
+//            $request->query->getInt('page', 1),
+//            $per_page
+//        );
+//        $totalItems = $pagination->getTotalItemCount();
+//
+//        $cmd_items = $pagination->getItems();
+//
+//        $orders = $this->ordersViewFactory->createOrderRows($cmd_items);
+
+        return $this->render('deliveries/index.html.twig', [
+        ]);
+    }
 
     #[Route('/{id}', name: 'app_deliveries_show', requirements: ['id' => '\d+'])]
     public function show(int $id): Response
@@ -52,4 +86,12 @@ class DeliveryController extends AbstractController
             'infos' => $infos,
         ]);
     }
+    #[Route('/orders/', name: 'app_deliveries_orders')]
+    public function listOrdersToDelivered(): Response
+    {
+        return $this->render('deliveries/orders.html.twig', []);
+    }
+
+
+
 }
